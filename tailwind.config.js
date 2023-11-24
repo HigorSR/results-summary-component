c4b5ed4bc212815fb735e1b0e2b379bb3daaa3a2
Higor Rocha <higor_36rocha@hotmail.com>
2023-11-24 05:25:10 -0300

    Configurações iniciais

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "light-slate-blue": "hsla(252, 100%, 67%)",
+        "light-royal-blue": "hsla(241, 81%, 54%)",
+        "violet-blue": "hsla(256, 72%, 46%, 1)",
+        "persian-blue": "hsla(241, 72%, 46%, 0)",
+        "Light-red": "hsl(0, 100%, 67%)",
+        "Orangey-yellow": "hsl(39, 100%, 56%)",
+        "Green-teal": "hsl(166, 100%, 37%)",
+        "Cobalt-blue": "hsl(234, 85%, 45%)",
+        "Pale-blue": "hsl(221, 100%, 96%)",
+        "light-lavender": "hsl(241, 100%, 89%)",
+        "Dark-gray-blue": "hsl(224, 30%, 27%)"
+      }
     },
   },
   plugins: [],
